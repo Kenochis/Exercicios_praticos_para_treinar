8dc141ded92e795e541513ddf836c700d3f99e2b
Kenochis <bragakaua968@gmail.com>
2025-10-30 18:49:31 -0300

    Exercícios

--- Exercicio_1/Media.js
+++ Exercicio_1/Media.js
@@ -3,8 +3,8 @@
 //Math.round() - arredonda o número para o inteiro mais próximo
 
 //função para calcular a média e situação do aluno
-function calcularMedia(notas, aluno){
-    let soma = (notas[0] + notas[1] + notas[2]) / 3; 
+function calcularMedia(aluno, notas){
+    let soma = (notas[0] + notas[1] + notas[2]) / 3; //calculando a soma das notas e dividindo por 3 para obter a média
     let media = Math.round(soma * 100) / 100; //arredondando a média para 2 casas decimais
 
     console.log("A média foi de: " + media);
@@ -20,5 +20,5 @@ function calcularMedia(notas, aluno){
     }
 }
 
-let notasAluno1 = [ [8, 7, 10], ["Kauã"] ]; //array com as notas e o nome do aluno
+let notasAluno1 = [aluno = "Kauã", notas = [8, 5, 10]]; //definindo o nome do aluno e suas notas
 calcularMedia(notasAluno1[0], notasAluno1[1]); //chamando a função com as notas e o nome do aluno
---
+++ Exercicio_3/Alternativa1.js
@@ -0,0 +1,31 @@
+//Loja de desconto para diferentes tipos de clientes
+
+function desconto(porcentagem, valor){ //recebe a porcentagem de desconto e o valor do produto
+
+    let descontoCliente = valor / 100 * porcentagem; 
+    let valorFinal = valor - descontoCliente; 
+    return valorFinal;
+}
+
+function clienteComum(ValorProduto){ //função para cliente comum
+    let cliente = [tipo = "Comum", porcentagem = 5];
+
+    console.log(`O produto no valor de ${ValorProduto} reais, com os ${cliente[1]}% de desconto do cliente ${cliente[0]}, ficou por apenas ${desconto(cliente[1], ValorProduto)} reais.`);
+}
+
+function clienteVip(ValorProduto){ //função para cliente vip
+    let cliente = [tipo = "Vip", porcentagem = 10];
+    
+    console.log(`O produto no valor de ${ValorProduto} reais, com os ${cliente[1]}% de desconto do cliente ${cliente[0]}, ficou por apenas ${desconto(cliente[1], ValorProduto)} reais.`);
+}
+
+function clienteFuncionario(ValorProduto){ //função para cliente funcionário
+    let cliente = [tipo = "Funcionário", porcentagem = 20];
+
+    console.log(`O produto no valor de ${ValorProduto} reais, com os ${cliente[1]}% de desconto do ${cliente[0]}, ficou por apenas ${desconto(cliente[1], ValorProduto)} reais.`);
+}
+
+//Testando as funções
+clienteComum(notebook = 10000); //valor do notebook = 10000 reais
+clienteVip(notebook = 10000);
+clienteFuncionario(notebook = 10000);
