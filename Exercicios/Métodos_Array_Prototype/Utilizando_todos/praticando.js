e4eef4a5a9cd5d327c2b0353c494b947edb09bb3
Kenochis <bragakaua968@gmail.com>
2025-11-27 18:00:19 -0300

    Exercicios

---
+++ Exercicios/Métodos_Array_Prototype/Utilizando_todos/praticando.js
@@ -0,0 +1,23 @@
+
+const produtos = [
+  { nome: "Pão", preco: 2.50 },
+  { nome: "Leite", preco: 4.00 }
+];
+
+// 1
+produtos.push({nome: "Café", preco: 5.00});
+/*----------------------------------------*/
+
+// 2
+produtos.forEach((produto) => console.log(produto));
+/*----------------------------------------*/
+
+// 3
+const nomes = produtos.map(produto => produto.nome);    // para retornar apenas uma coisa, não precisa das {}.
+console.log(nomes);
+/*----------------------------------------*/
+
+// 4
+const valores = produtos.filter(produto => produto.preco >= 4 );
+
+console.log(valores);
