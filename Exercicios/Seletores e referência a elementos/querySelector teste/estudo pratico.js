74a38ae0bcab63e267cfd652eda353a55db76b6e
Kenochis <bragakaua968@gmail.com>
2025-12-01 18:48:52 -0300

    DOM e Eventos

---
+++ Exercicios/Seletores e referência a elementos/querySelector teste/estudo pratico.js
@@ -0,0 +1,15 @@
+
+const item1 = document.querySelector('h1');
+
+console.log(item1);
+
+const item2 = document.querySelector('p');
+
+console.log(item2);
+
+const item3 = document.querySelector('#btn2');
+
+console.log(item3);
+
+const imprimir = document.querySelectorAll('p')
+.forEach(impri => console.log(impri));
