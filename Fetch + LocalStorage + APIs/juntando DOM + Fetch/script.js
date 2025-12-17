0558699aaf29cbaa560dd8c8466b111c3e492e51
Kenochis <bragakaua968@gmail.com>
2025-12-17 18:01:58 -0300

    Incompleto

---
+++ Fetch + LocalStorage + APIs/juntando DOM + Fetch/script.js
@@ -0,0 +1,5 @@
+// primeiro passo - input busca na API
+// segundo passo - se não encontrar, aparece uma mensagem
+// terceiro passo - btn excluir e btnFavoritar
+// quarto passo - botão para abrir a lista de favoritos (com os cards inteiros)
+// quinto passo - botão de remover dos favoritos
